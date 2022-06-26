a881a1c2f4798bc58a1e72e225f2adc7eb56b4ee
Ian Lee <vanilla.punches@gmail.com>
2022-06-26 17:05:44 -0400

    1. Basic info about databases, table types, status

---
+++ 1_Datadases_Table_Status_Types.sql
@@ -0,0 +1,7 @@
+SHOW DATABASES;
+USE world;
+SHOW TABLES;
+SHOW TABLE STATUS; # Describe Table information
+DESCRIBE city;  # Describe city's types, key, etc.     DESCRIBE = DESC
+DESC country;
+DESC countrylanguage;
